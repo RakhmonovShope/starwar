cd7b3009db2ead60a1199b90b4d1d367421ecb3b
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-21 11:37:24 +0500

    feat(Character): styles changed

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -1,37 +1,48 @@
 import React from 'react';
 
 import classes from './Character.module.scss';
-import { IEntity } from '../../modules/character/types.ts';
 
 interface IProps {
-  character: IEntity.Person;
-  onSelect: (character: IEntity.Person) => void;
+  id: number;
+  name: string;
+  gender: string;
+  height: string;
+  mass: string;
+  onSelect: (id: string) => void;
 }
 
-const Character: React.FC<IProps> = ({ character, onSelect }) => {
-  const imageUrl = `https://starwars-visualguide.com/assets/img/characters/${character.id}.jpg`;
+const Character: React.FC<IProps> = ({
+  id,
+  name,
+  gender,
+  height,
+  mass,
+  onSelect,
+}) => {
+  const imageUrl = `https://starwars-visualguide.com/assets/img/characters/${id}.jpg`;
 
   return (
-    <div className={classes.wrapper} onClick={() => onSelect(character)}>
-      <img
-        src={imageUrl}
-        alt={character.name}
-        className={classes.characterImage}
-        onError={(e) => {
-          (e.target as HTMLImageElement).src =
-            'https://starwars-visualguide.com/assets/img/placeholder.jpg';
-        }}
-      />
+    <div className={classes.wrapper} onClick={() => onSelect(id)}>
+      <div className={classes.image}>
+        <img
+          src={imageUrl}
+          alt={name}
+          onError={(e) => {
+            (e.target as HTMLImageElement).src =
+              'https://starwars-visualguide.com/assets/img/placeholder.jpg';
+          }}
+        />
+      </div>
       <div className={classes.info}>
-        <h3>{character.name}</h3>
+        <h3>{name}</h3>
         <p>
-          <strong>Gender:</strong> {character.gender}
+          <strong>Gender:</strong> {gender}
         </p>
         <p>
-          <strong>Height:</strong> {character.height} cm
+          <strong>Height:</strong> {height} cm
         </p>
         <p>
-          <strong>Mass:</strong> {character.mass} kg
+          <strong>Mass:</strong> {mass} kg
         </p>
       </div>
     </div>
