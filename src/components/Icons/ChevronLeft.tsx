5d8d87617ad109120c4e752f7aed73347b2e5e73
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-21 10:18:57 +0500

    feat: icons components added

---
+++ src/components/Icons/ChevronLeft.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ChevronLeft: React.FC = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="32" height="32" rx="4" fill="white" fill-opacity="0.4" />
+    </svg>
+  );
+};
+
+export default ChevronLeft;
